7b7a289b64930559b6131d00df80eb5fa9bae928
Gerson Rodrigues <gerson.rodrigues@gmail.com>
2018-06-04 16:30:50 -0300

    Add files via upload

--- src/test/java/sofia/toolbox/msoffice/testcase/ExcelTest.java
+++ src/test/java/sofia/toolbox/msoffice/testcase/ExcelTest.java
@@ -8,10 +8,10 @@ import java.io.IOException;
 import org.junit.Test;
 
 import sofia.toolbox.io.FileSystem;
-import sofia.toolbox.msoffice.Excel97;
+import sofia.toolbox.msoffice.Excel;
 
 
-public class Excel97Test {
+public class ExcelTest {
 	
 	String currentDir = new FileSystem().getCurrentDirectory() + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator;	
 
@@ -20,19 +20,19 @@ public class Excel97Test {
 		
 		File file = new File(currentDir + "Resultado.xls");
 		if (file.exists()) file.delete();
-		
-        Excel97 excel = new Excel97();
+
+        Excel excel = new Excel();
         excel.createWorkbook("Resultado.xls", new String[]{"Nome Documento"}, new int[]{8000});
 
         excel.createRow();
-        excel.createColumn("ADP2-VT3-DFH-00002_B.DOC", Excel97.ALIGN_LEFT);
+        excel.createColumn("ADP2-VT3-DFH-00002_B.DOC", Excel.ALIGN_LEFT);
 
         excel.createRow();
-        excel.createColumn("ADP2-VT3-DFH-00002_C.DOC", Excel97.ALIGN_LEFT);
+        excel.createColumn("ADP2-VT3-DFH-00002_C.DOC", Excel.ALIGN_LEFT);
         
-        excel.setCellValue(15, 15, "Gerson", Excel97.ALIGN_CENTER);
+        excel.setCellValue(15, 15, "Gerson", Excel.ALIGN_CENTER);
 
-        excel.save("src/test/resources/resultado.xls") ;
+        excel.save("src/test/resources/resultado.xlsx") ;
 		assertTrue("Value of Cell (0, 0) is wrong", excel.getStringCellValue(0,  0).equals("Nome Documento"));
 		assertTrue("Value of Cell (0, 1) is wrong", excel.getStringCellValue(0,  1).equals("ADP2-VT3-DFH-00002_B.DOC"));
 		assertTrue("Value of Cell (0, 2) is wrong", excel.getStringCellValue(0,  2).equals("ADP2-VT3-DFH-00002_C.DOC"));	
---
+++ src/test/java/sofia/toolbox/msoffice/testcase/package-info.java
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+/**
+ * 
+ * @author Gerson Rodrigues Magalhaes
+ * @author <a target="_new" href="http://gersonweb.com">http://gersonweb.com</a>
+ * @version 2.0
+ *
+ */
+package sofia.toolbox.msoffice.testcase;
